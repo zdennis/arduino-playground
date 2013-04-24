65881e304c7e3b1fec9dc9db7752017f3c7eb7d3
Zach Dennis <zach.dennis@gmail.com>
2013-04-23 21:22:24 -0400

    Whoo-hoo! Got all four buttons working

--- button_led_matix/sketch1/sketch1.ino
+++ button_led_matix/sketch1/sketch1.ino
@@ -1,31 +1,31 @@
-/*
+ /*
 +-------+-------+
 |       |       |
-|   3   |   4   |
+|   1   |   3   |
 |       |       |--
 +-------+-------+--
 |       |       |--
-|   2   |   1   |--
+|   2   |   4   |--
 |       |       |
 +-------+-------+
 */
 
 // Button Grounds
-const int buttonGround1 = 12; //13;
-//const int buttonGround2 = 2;
-//const int buttonGround3 = 11;
-//const int buttonGround4 = 8;
+const int buttonGround1 = 12;
+const int buttonGround2 = 7;
+const int buttonGround3 = 13;
+const int buttonGround4 = 9;
 
 // LED Grounds
-const int ledGround1    = 11;
-const int ledGround2    = 8;
-const int ledGround3    = 10;
-const int ledGround4    = 2;
+const int ledGround1    = 10;
+const int ledGround2    = 11;
+const int ledGround3    = 2;
+const int ledGround4    = 8;
 
 // RGB Pins
 const int redLED        = 3;
-const int greenLED      = 5;
-const int blueLED       = 6;
+const int greenLED      = 6;
+const int blueLED       = 5;
 
 // Color definitions
 int red[] = {255, 0, 0};
@@ -38,9 +38,9 @@ int dark[] = {0, 0, 0};
 void setup(){
   // Switch grounds
   pinMode(buttonGround1, INPUT);
-//  pinMode(buttonGround2, INPUT);
-//  pinMode(buttonGround3, INPUT);
-//  pinMode(buttonGround4, INPUT);
+  pinMode(buttonGround2, INPUT);
+  pinMode(buttonGround3, INPUT);
+  pinMode(buttonGround4, INPUT);
 
   // LED Grounds
   pinMode(ledGround1, OUTPUT);
@@ -53,8 +53,8 @@ void setup(){
   pinMode(greenLED, OUTPUT);
   pinMode(blueLED,  OUTPUT);
 
-   Serial.begin(9600);           // set up Serial library at 9600 bps
-   Serial.println("Starting...");  // prints hello with ending line break     
+  //Serial.begin(9600);           // set up Serial library at 9600 bps
+  //Serial.println("Starting...");  // prints hello with ending line break     
 }
 
 const int ON = LOW;
@@ -66,40 +66,71 @@ int interval = 40;
 int state = 0;
 
 void loop(){
-  int old_state = state;
-  
-  if(counter == interval*4){
-    counter = 0;
-  }
-  
-  if(counter >= 0 && counter < interval){
-    state = 1;
+  int btn1 = digitalRead(buttonGround1);
+  int btn2 = digitalRead(buttonGround2);
+  int btn3 = digitalRead(buttonGround3);
+  int btn4 = digitalRead(buttonGround4);
+
+  setFourLEDs(dark, dark, dark, dark);
+
+ // delay(100);
+  if(btn1 == HIGH){
     setLED(ledGround1, red);
-    setLED(ledGround2, green);
-    setLED(ledGround3, blue);
-  } else if(counter >= interval*1 && counter <= interval*2){
-    state = 2;
-    setLED(ledGround1, green);
-    setLED(ledGround2, blue);
-    setLED(ledGround3, red);
-  } else if(counter >= interval*2){
-    state = 3;
-    setLED(ledGround1, blue);
-    setLED(ledGround2, red);
-    setLED(ledGround3, green);
+//    setFourLEDs(red, dark, dark, dark);
   }
+  if(btn2 == HIGH){
+    setLED(ledGround2, green);
+//    setFourLEDs(dark, green, dark, dark);
+  }
+  if(btn3 == HIGH){
+    setLED(ledGround3, blue);
+  //  setFourLEDs(dark, dark, blue, dark);
+  }
+  if(btn4 == HIGH){
+    setLED(ledGround4, yellow);
+//    setFourLEDs(dark, dark, dark, yellow);
+  }
+
   
-  if(old_state != state){
+//  int old_state = state;
+//  
+//  if(counter == interval*4){
+//    counter = 0;
+//  }
+  
+//    setLED(ledGround1, red);
+//    setLED(ledGround2, red);
+//    setLED(ledGround3, red);
+//    setLED(ledGround4, red);
+//  
+//  if(counter >= 0 && counter < interval){
+//    state = 1;
+//    setLED(ledGround1, red);
+//    setLED(ledGround2, green);
+//    setLED(ledGround3, blue);
+//  } else if(counter >= interval*1 && counter <= interval*2){
+//    state = 2;
+//    setLED(ledGround1, green);
+//    setLED(ledGround2, blue);
+//    setLED(ledGround3, red);
+//  } else if(counter >= interval*2){
+//    state = 3;
+//    setLED(ledGround1, blue);
+//    setLED(ledGround2, red);
+//    setLED(ledGround3, green);
+//  }
+  
+ // if(old_state != state){
     //Serial.begin(9600);           // set up Serial library at 9600 bps
   //  Serial.println("color change occurred");  // prints hello with ending line break     
-  }
+ // }
   
-  Serial.print(digitalRead(buttonGround1));
+ // Serial.print(digitalRead(buttonGround1));
 //     Serial.println("button press occurred");  // prints hello with ending line break     
 //  }
   
-  delay(1000);
-  counter += 1;
+ // delay(1000);
+ // counter += 1;
 }
 
 void setLED(int ledPin, int rgbColor[]){
@@ -111,5 +142,12 @@ void setLED(int ledPin, int rgbColor[]){
   
   delayMicroseconds(1100);
   digitalWrite(ledPin, OFF);
+}
+
+void setFourLEDs(int colorOne[], int colorTwo[], int colorThree[], int colorFour[]){
+  setLED(ledGround1, colorOne);
+  setLED(ledGround2, colorTwo);
+  setLED(ledGround3, colorThree);
+  setLED(ledGround4, colorFour);
 }
 
